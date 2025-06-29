415db8287ee196782057d4cc5725745b770f749b
bun <lolmebunbun@gmail.com>
2025-06-29 02:39:12 -0500

    Create Ship class

---
+++ src/modules/ship.js
@@ -0,0 +1,19 @@
+class Ship {
+  constructor() {
+    this.length_ = 0;
+    this.hit_ = 0;
+    this.sunk_ = false;
+  }
+
+  hit() {
+    this.hit += 1;
+
+    this.isSunk();
+  }
+
+  isSunk() {
+    if (this.hit >= this.length) {
+      this.sunk = false;
+    }
+  }
+}
