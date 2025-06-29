f5fb7f601c192d6bd40ffe7aad4e87aa6faf4c32
bun <lolmebunbun@gmail.com>
2025-06-29 02:48:34 -0500

    Create GameBoard class Template

---
+++ src/modules/gameBoard.js
@@ -0,0 +1,32 @@
+class GameBoard {
+  constructor() {
+    this.rows = 8;
+    this.cols = 8;
+
+    this.board = this.createBoard(this.rows, this.cols);
+
+    this.missed = 0;
+  }
+  // Create Board
+  // Returns board
+  createBoard() {}
+
+  // Prints the board
+  printBoard() {}
+
+
+  // Determine if all ships are sunk 
+  // Returns true if all ships sunk & false if not
+  gameOver(){
+
+  }
+
+
+  // Places a ship in the coordinates
+  // Returns true if placed & false if coordinate is occupied
+  placeShip() {}
+
+  // Receive apair of coordinates and sends the hit function of the ship or coordinates of the missed shot
+  // Returns true if hit and false if missed
+  attackShip(x, y) {}
+}
