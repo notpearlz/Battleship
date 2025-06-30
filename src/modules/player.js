15925d5653767c97376b94e63274b3b255de8901
bun <lolmebunbun@gmail.com>
2025-06-30 05:01:05 -0500

    Add getters & setters for player.js

--- src/modules/player.js
+++ src/modules/player.js
@@ -1,8 +1,24 @@
-class Player{
-    constructor(){
-        this.type = null;
-        this.gameBoard = null;
-    }
+class Player {
+  constructor() {
+    this.type = null;
+    this.gameBoard = null;
+  }
+
+  get type() {
+    return this.type;
+  }
+
+  get board() {
+    return this.gameBoard;
+  }
+
+  set type(newType) {
+    this.type = newType;
+  }
+
+  set board(newBoard) {
+    this.type = newBoard;
+  }
 }
 
-export {Player};
+export { Player };
